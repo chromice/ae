b48e77ae79827c4c9111a8aa4e91c0c575f2a586
Anton Muraviev <chromice@gmail.com>
2011-06-18 18:21:05 +0100

    Better aeProcess API.

--- examples/process/index.php
+++ examples/process/index.php
@@ -2,7 +2,7 @@
 
 $process = new aeProcess('image-resize');
 
-$job = $process->job('some_image.png?w=200&h=200')
+$job = $process->job('thumbnail/some_image.png?w=200&h=200')
 	->delay(50)
 	->priority(200);
 
@@ -13,14 +13,23 @@ $job = $process->job(); // next job
 $job->id;
 $job->data; // raw data
 
-// Jobs passed as URL can be parsed with parse_url()
-$host = $job->url(); // full url; or schema(), user(), pass(), path(), query([name]) or fragment()
-$base_url = $job->url('schema','host'); // schema+host
-$x = $job->query('w'); // $x = 200;
-$y = $job->query('h'); // $y = 200;
+if ($job->format() !== 'json') // or ($job->format() === 'path')
+{
+	// Path format
+	$path = $job->path(); // "thumbnail/some_image.png"
+	$x = $job->paremeter('w'); // $x = 200;
+	$y = $job->paremeter('h'); // $y = 200;
+} 
+else
+{
+	// JSON format
+	$data = $job->decode();
+}
 
-// $job->delay(100);
-// $queue->queue($job);
+
+
+$job->delay(100);
+$queue->queue($job);
 
 // $queue->bury($job);
 // $queue->kick(1);
