80701f942bf43857dff3a05710aac863886acfa6
Anton Muraviev <chromice@gmail.com>
2011-06-24 23:17:09 +0100

    More pipeline comments and ideas.

--- examples/pipelines/index.php
+++ examples/pipelines/index.php
@@ -1,10 +1,11 @@
 <?php
 
 /*
-	This is simple Beanstalkd abstraction I will some day use.
+	This is simple Beanstalkd abstraction I will use some day.
 */
 
 $pipe = new aePipeline('image-resize');
+// $pipe will use tube 'image-resize'
 
 // $job = $process->job(array())
 $job = $pipe->job('thumbnail/some_image.png?w=200&h=200')
@@ -15,8 +16,13 @@ $pipe->push($job);
 
 $job = $pipe->pop(); // get next job from the queue
 
-$job->id;
+$job->id;	// a Beantalkd ID
 $job->data; // 'thumbnail/some_image.png?w=200&h=200'
+
+// A nice to have feature.
+$stats = $pipe->command('stats'); // returns an associative array.
+// OR
+$tube_stats = $pipe->command('stats-tube image-resize'); // same but for this tube
 
 $url = $job->url();
 // OR 
