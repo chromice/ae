6a799697d5822c8b9d6215e099c2164e9f9d7509
Anton Muraviev <chromice@gmail.com>
2011-07-02 14:42:14 +0100

    Added a new approach to routing to request example.

--- examples/request/index.php
+++ examples/request/index.php
@@ -1,11 +1,14 @@
 <?php
 
 $request = ae::load('request.php');
+// or
+$request = ae::load('request.php','index.php/segment-1/segment-2/segment-3?query=string');
+// to specify uri manually
 
 /*
 	web or ajax request: index.php/segment-1/segment-2/segment-3?query=string
 	
-	cli request: index.php segment-1 segment-2 segment-3
+	cli request: index.php segment-1 segment-2 segment-3 --query=string
 */
 
 if ($request->is('ajax')) // or 'cli', or 'normal'
@@ -17,22 +20,28 @@ echo $request->segment(0); // returns "index.php" or whatever file was called fi
 echo $request->segment(1,'default'); // returns "segment-1"
 echo $request->segment(3,'default'); // returns "default"
 
-
-$route = ae::load('route.php',$request->uri())
-	->base('/request')
-	->alias('uri/path','real/path');
+$route = $request->route('base/path', array(
+	'alias/1' => 'some/path',
+	'alias/2' => 'some/other/path'
+));
 
 if ($route->exists())
 {
-	// Load path for $request->uri()
-	ae::load($route->path());
+	echo $route->argument(0); // 'segment-1'
+	
+	ae::render($route->path()); 
+	// loads a script, e.g. '/segment-1/segment-2.php' and
+	// shifts the argument pointer offset from 'segment-1' to 'segment-3'
+	
+	echo $route->argument(0); // 'segment-3'
+	
+	$route2 = $request->route(); // returns the copy of the perviously created route object
+	
+	echo $route2->argument(0); // 'segment-3'
+	echo $route2->argument(1,'default'); // 'default'
 }
 else
 {
 	ae::render('errors/404.php', array('uri' => $request->uri()));
 }
-
-
-
-
 
