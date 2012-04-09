990d599cb0d244e5c9483bbc3ec106427ee04bd1
Anton Muraviev <chromice@gmail.com>
2012-04-09 22:15:57 +0100

    Cleaned up response library and fixed JSON mime string.

--- ae/response.php
+++ ae/response.php
@@ -32,55 +32,55 @@ class aeResponse
 	protected $cache_private;
 	
 	protected $http_statuses = array(
-		100 => '100 Continue',
-		101 => '101 Switching Protocols',
+		100 => 'Continue',
+		101 => 'Switching Protocols',
 
 		//  Success
-		200 => '200 OK',
-		201 => '201 Created',
-		202 => '202 Accepted',
-		203 => '203 Non-Authoritative Information',
-		204 => '204 No Content',
-		205 => '205 Reset Content',
-		206 => '206 Partial Content',
+		200 => 'OK',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
 
 		//  Redirects
-		300 => '300 Multiple Choices',
-		301 => '301 Moved Permanently',
-		302 => '302 Found',
-		303 => '303 See Other',
-		304 => '304 Not Modified',
-		305 => '305 Use Proxy',
-		// 306 => '306 (Unused)',
-		307 => '307 Temporary Redirect',
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found',
+		303 => 'See Other',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		// 306 => '(Unused)',
+		307 => ' Temporary Redirect',
 
 		// Client errors
-		400 => '400 Bad Request',
-		401 => '401 Unauthorized',
-		402 => '402 Payment Required',
-		403 => '403 Forbidden',
-		404 => '404 Not Found',
-		405 => '405 Method Not Allowed',
-		406 => '406 Not Acceptable',
-		407 => '407 Proxy Authentication Required',
-		408 => '408 Request Timeout',
-		409 => '409 Conflict',
-		410 => '410 Gone',
-		411 => '411 Length Required',
-		412 => '412 Precondition Failed',
-		413 => '413 Request Entity Too Large',
-		414 => '414 Request-URI Too Long',
-		415 => '415 Unsupported Media Type',
-		416 => '416 Requested Range Not Satisfiable',
-		417 => '417 Expectation Failed',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
 
 		// Server errors
-		500 => '500 Internal Server Error',
-		501 => '501 Not Implemented',
-		502 => '502 Bad Gateway',
-		503 => '503 Service Unavailable',
-		504 => '504 Gateway Timeout',
-		505 => '505 HTTP Version Not Supported'
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported'
 	);
 	
 	public function __construct()
@@ -101,7 +101,7 @@ class aeResponse
 			unset($this->buffer);
 			$this->buffer = new aeBuffer();
 		}
-		
+
 		$this->headers = array();
 		
 		return $this->status(200)
@@ -124,7 +124,7 @@ class aeResponse
 		if ($this->status !== 200 && isset($this->http_statuses[$this->status]))
 		{
 			$protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1';
-			header($protocol . ' ' . $this->http_statuses[$this->status]);
+			header($protocol . ' ' . $this->status . ' ' . $this->http_statuses[$this->status]);
 		}
 		
 		// Get buffered output and destroy the buffer
@@ -222,23 +222,31 @@ class aeResponse
 		switch ($type)
 		{
 			case 'html':
-				$this->type = 'text/html'; break;
+				$this->type = 'text/html';
+				break;
 			case 'css':
 			case 'stylesheet':
-				$this->type = 'text/css'; break;
-			case 'javascript':
-			case 'json':
+				$this->type = 'text/css';
+				break;
 			case 'js':
-				$this->type = 'text/javascript'; break;
+			case 'javascript':
+				$this->type = 'text/javascript';
+				break;
 			case 'text':
-				$this->type = 'text/plain'; break;
+				$this->type = 'text/plain';
+				break;
+			case 'json':
+				$this->type = 'application/json';
+				break;
 			case 'atom':
 			case 'rdf':
 			case 'rss':
 			case 'xhtml':
-				$this->type = 'application/'.$type.'+xml'; break;
+				$this->type = 'application/'.$type.'+xml';
+				break;
 			case 'xml':
-				$this->type = 'application/xml'; break;
+				$this->type = 'application/xml';
+				break;
 			default:
 				$this->type = $type;
 		}
