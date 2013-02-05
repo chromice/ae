ecde898e5dd57c4cd8b7361030c193c26ea3e594
Anton Muraviev <chromice@gmail.com>
2013-02-05 11:38:04 +0000

    Nailed the multiple fields problem.

--- examples/forms/basic.php
+++ examples/forms/basic.php
@@ -10,15 +10,15 @@ if ($form->is_submitted())
 		->required('Please enter some text')
 		->max_length(140, 'Let\'s keep it short, shall we?');
 		
-	$form->validate('textarea', aeField::multiple);
+	$form->validate('textarea', aeForm::sequence); // the last field is validated only if not empty
 	
-	$form->validate('select', aeField::single)
+	$form->validate('select', aeForm::single)
 		->options(array('foo','bar')) // prevent user from supplying wrong values
 		->required('How did you manage to select nothing?!');
 	
-	$form->validate('check', aeField::multiple)
+	$form->validate('check', aeForm::multiple) // field is validated only if not empty
 		->options(array('foo' => 'Foo', 'bar' => 'Bar')) // keys are used here
-		->required('Please check at least one box!');
+		->required('Please check at least one box!'); // at least one value is required by default
 		
 	if ($form->is_valid())
 	{
@@ -54,14 +54,14 @@ if ($form->is_submitted())
 	<input name="text" id="text-input" type="text" value="<?php $form->value('text') ?>">
 	<?php $form->error('text') ?>
 </div>
-<?php while ($form->has('textarea', 1)): ?>
+<?php while ($form->has('textarea', 0)): ?>
 <div class="field <?php $form->classes('textarea') ?>">
-	<label for="textarea-input">Text input:</label>
+	<label for="textarea-input">Text area:</label>
 	<?php $form->error('textarea') ?>
 	<textarea name="textarea" id="textarea"><?php $form->value('textarea') ?></textarea>
 </div>
 <?php endwhile ?>
-<p class="ae-more"><?php $form->add('textarea', 'Add one more') ?> text input.</p>
+<p><?php $form->add('textarea', 'Add', 'Add one more') ?> text area.</p>
 <div class="field <?php $form->classes('select') ?>">
 	<label for="select-input">Select something:</label>
 	<select name="select" id="select-input">
@@ -70,13 +70,13 @@ if ($form->is_submitted())
 	</select>
 	<?php $form->error('select') ?>
 </div>
-<div class="field<?php $form->classes('check') ?>">
+<div class="field <?php $form->classes('check') ?>">
 	<label>Checkboxes:</label>
 	<label><input type="checkbox" name="check[]" value="foo" <?php $form->checked('check', 'foo') ?>>foo</label>
 	<label><input type="checkbox" name="check[]" value="bar" <?php $form->checked('check', 'bar') ?>>bar</label>
 	<?php $form->error('check') ?>
 </div>
 <div class="field">
-	<input value="Submit" type="submit">
+	<?php $form->submit('Submit') ?>
 </div>
 <?php $form->close() ?>
