83cf3030d39808340702cac66877c07851ae27c4
Anton Muraviev <chromice@gmail.com>
2013-02-13 21:25:03 +0000

    More straightforward database migration example.

--- examples/database/migration.php
+++ examples/database/migration.php
@@ -1,26 +1,22 @@
 <?php 
 
-$table = "some_new_table";
-
 $initialise = ae::database()
-	->table($table)
-	->create("
-		CREATE TABLE {table} (
+	->initialise("some_new_table", 
+		"CREATE TABLE {table} (
 		  `id` int(10) NOT NULL auto_increment,
 		  `name` varchar(255) NOT NULL default '',
 		  `private_memgroup` tinyint(4) default '0' COMMENT 'determine whether the document group is private to manager users',
 		  `private_webgroup` tinyint(4) default '0' COMMENT 'determines whether the document is private to web users',
 		  PRIMARY KEY  (`id`),
 		  UNIQUE KEY `name` (`name`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Contains data used for access permissions.';
-	");
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Contains data used for access permissions.'")
+	->commit();
 
-$remove_data_rows = ae::database()
-	->state($initialise)
-	->apply("
-		UPDATE TABLE {table} DROP COLUMN `private_memgroup`;
-	");
+$remove_data_rows = $initialise
+	->update('UPDATE TABLE {table} DROP COLUMN `private_memgroup`')
+	->update('UPDATE TABLE {table} DROP COLUMN `private_webgroup`')
+	->commit();
 
-$table_no_more = ae::database()
-	->table($table) // or ->state($add_more_data_rows)
-	->drop();
+$table_no_more = $remove_data_rows
+	->update('DROP TABLE {table}')
+	->commit();
