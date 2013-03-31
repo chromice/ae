ffdc5a2af36130a5de477c26153334017e73f216
Anton Muraviev <chromice@gmail.com>
2013-03-31 18:18:04 +0100

    Commented the form example… before I refactor it.

--- examples/form.php
+++ examples/form.php
@@ -2,8 +2,13 @@
 
 $form = ae::form('form-id');
 
-$form->values(array('select' => 'bar'));
+// Set default values
+if (!$form->is_posted())
+{
+	$form->values(array('select' => 'bar'));
+}
 
+// Create a single field that accepts integers from 0 to 1000
 $number = $form->single('text')
 	->required('Please enter some number.')
 	->format('Please enter a valid integer number.', aeForm::valid_integer)
@@ -16,6 +21,7 @@ $number = $form->single('text')
 	->max_value('Cannot be higher then a thousand.', 1000);
 
 
+// Create a sequence of 0 to 5 fields that accept tweet size chunks of text
 $textarea = $form->sequence('textarea', 0, 5) // the last field is validated only if not empty
 	->required('Please enter some text.')
 	// ->format('Must be a valid email, e.g. "some.dude@example.com".', aeForm::valid_email)
@@ -27,15 +33,17 @@ $textarea = $form->sequence('textarea', 0, 5) // the last field is validated onl
 	->min_length('Just type a few characters!', 2)
 	->max_length('Let\'s keep it short, shall we?', 140);
 
+// Create a single field, that accepts only 'foo' and 'bar' values
 $select = $form->single('select')
 	->required('How did you manage to select nothing?!')
 	->options(array('foo','bar')); // prevent user from supplying wrong values
 
+// Create a single field, that accepts an array of 'foo' and 'bar' values
 $checkboxes = $form->multiple('check') // field is validated only if not empty
 	->required('Please check at least one box!') // at least one value is required by default
 	->options(array('foo' => 'Foo', 'bar' => 'Bar')); // keys are used here
 
-// Process special actions
+// Try processing all special actions first
 if ($form->value('add'))
 {
 	$textarea[] = ''; // Empty by default
@@ -44,11 +52,13 @@ else if ($index = $form->value('remove'))
 {
 	unset($textarea[$index]);
 }
-// Process the form only if no other actions are made
+// Process the form only if no other actions are made, and the form is posted
 else if ($form->is_posted())
 {
+	// Run the validation, which will set errors
 	$is_valid = $form->validate();
 	
+	// Custom validation goes here
 	if (empty($text->error) && $form->value('number') === '666')
 	{
 		$text->error = 'Devils are not allowed!';
@@ -56,6 +66,7 @@ else if ($form->is_posted())
 		$is_valid = false;
 	}
 	
+	// If form is valid, do something with it.
 	if ($is_valid)
 	{
 		$values = $form->values();
