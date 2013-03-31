2fd3af5ff27b5d5e429b4d1d930c7eb40ff02fac
Anton Muraviev <chromice@gmail.com>
2013-03-31 05:24:03 +0100

    Even simple form example. When will I build the library?

--- examples/form.php
+++ examples/form.php
@@ -4,19 +4,24 @@ $form = ae::form('form-id');
 
 $form->values(array('select' => 'bar'));
 
-$text = $form->field('text')
+$text = $form->single('text')
 	->required('Please enter some text')
 	->length('Let\'s keep it short, shall we?', 140);
-	
-$textareas = $form->sequence('textarea', 0, 5); // the last field is validated only if not empty
 
-$select = $form->field('select')
-	->options(array('foo','bar')) // prevent user from supplying wrong values
-	->required('How did you manage to select nothing?!');
+$textarea = $form->sequence('textarea', 0, 5); // the last field is validated only if not empty
+
+if ($form->value('action') === 'add')
+{
+	$textarea->increment();
+}
+
+$select = $form->single('select')
+	->required('How did you manage to select nothing?!')
+	->options(array('foo','bar')); // prevent user from supplying wrong values
 
 $checkboxes = $form->multiple('check') // field is validated only if not empty
-	->options(array('foo' => 'Foo', 'bar' => 'Bar')) // keys are used here
-	->required('Please check at least one box!'); // at least one value is required by default
+	->required('Please check at least one box!') // at least one value is required by default
+	->options(array('foo' => 'Foo', 'bar' => 'Bar')); // keys are used here
 	
 if ($form->is_valid())
 {
@@ -48,22 +53,22 @@ else
 <?= $form->open() ?>
 <div class="field <?= $text->classes() ?>">
 	<label for="text-input">Text input:</label>
-	<input name="text" id="text-input" type="text" value="<?= $text->value() ?>">
+	<input name="<?= $text->name() ?>" id="text-input" type="text" value="<?= $text->value() ?>">
 	<?= $text->error('<em class="error">', '</em>') ?>
 </div>
-<?php while ($textarea = $textareas->next()): ?>
-<div class="field <?= $textarea>classes() ?>">
-	<label for="textarea-input">Text area:</label>
-	<textarea name="textarea[<?= $textarea->index() ?>]" id="textarea"><?= $textarea->value() ?></textarea>
-	<?= $textarea->error() ?>
+<?php foreach ($textarea as $_ta): ?>
+<div class="field <?= $_ta>classes() ?>">
+	<label>Text area:</label>
+	<textarea name="<?= $_ta->name() ?>[<?= $_ta->index() ?>]"><?= $_ta->value() ?></textarea>
+	<?= $_ta->error() ?>
 </div>
-<?php endwhile ?>
-<?php if ($textareas->count() < 5): ?>
-<p><?= $form->add('textarea', 'Add', 'Add one more') ?> text area.</p>
+<?php endforeach ?>
+<?php if ($textarea->count() < 5): ?>
+<p><button type="submit" name="action" value="add">Add</button> textarea.</p>
 <?php endif ?>
 <div class="field <?= $select->classes() ?>">
 	<label for="select-input">Select something:</label>
-	<select name="select" id="select-input">
+	<select name="<?= $select->name() ?>" id="select-input">
 		<option value="foo" <?= $select->selected('foo') ?>>Foo</option>
 		<option value="bar" <?= $select->selected('bar') ?>>Bar</option>
 	</select>
@@ -71,11 +76,11 @@ else
 </div>
 <div class="field <?= $checkboxes->classes() ?>">
 	<label>Checkboxes:</label>
-	<label><input type="checkbox" name="check[]" value="foo" <?= $checkboxes->checked('foo') ?>>foo</label>
-	<label><input type="checkbox" name="check[]" value="bar" <?= $checkboxes->checked('bar') ?>>bar</label>
+	<label><input type="checkbox" name="<?= $checkboxes->name() ?>[]" value="foo" <?= $checkboxes->checked('foo') ?>>foo</label>
+	<label><input type="checkbox" name="<?= $checkboxes->name() ?>[]" value="bar" <?= $checkboxes->checked('bar') ?>>bar</label>
 	<?= $checkboxes->error() ?>
 </div>
 <div class="field">
-	<?= $form->submit('Submit') ?>
+	<button type="submit">Submit</button>
 </div>
 <?= $form->close() ?>
