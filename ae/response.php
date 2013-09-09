00c466abeca4b9f37d5e585ebcb28144efb92091
Anton Muraviev <chromice@gmail.com>
2013-09-09 20:43:58 +0100

    Remove error codes from error statuses.

--- ae/response.php
+++ ae/response.php
@@ -105,32 +105,32 @@ class aeResponse
 	
 	protected $http_errors = array(
 		// Client errors
-		400 => '400 Bad Request',
-		401 => '401 Unauthorized',
-		402 => '402 Payment Required',
-		403 => '403 Forbidden',
-		404 => '404 Not Found',
-		405 => '405 Method Not Allowed',
-		406 => '406 Not Acceptable',
-		407 => '407 Proxy Authentication Required',
-		408 => '408 Request Timeout',
-		409 => '409 Conflict',
-		410 => '410 Gone',
-		411 => '411 Length Required',
-		412 => '412 Precondition Failed',
-		413 => '413 Request Entity Too Large',
-		414 => '414 Request-URI Too Long',
-		415 => '415 Unsupported Media Type',
-		416 => '416 Requested Range Not Satisfiable',
-		417 => '417 Expectation Failed',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
 		
 		// Server errors
-		500 => '500 Internal Server Error',
-		501 => '501 Not Implemented',
-		502 => '502 Bad Gateway',
-		503 => '503 Service Unavailable',
-		504 => '504 Gateway Timeout',
-		505 => '505 HTTP Version Not Supported'
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported'
 	);
 	
 	public function error($code, $path = null)
@@ -154,7 +154,7 @@ class aeResponse
 		// Respond with a correct header
 		header((isset($_SERVER['SERVER_PROTOCOL']) 
 			? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1')
-			. ' ' . $this->http_errors[$code]);
+			. ' ' . $code . ' ' . $this->http_errors[$code]);
 		
 		if (empty($path)) 
 		{
