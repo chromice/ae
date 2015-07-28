bb6f8d3b8f61f320054c0a338f79376704e3ed8f
Anton Muraviev <chromice@gmail.com>
2015-07-28 16:26:14 +0100

    Add (back) automatic response dispatching.

--- ae/response.php
+++ ae/response.php
@@ -114,13 +114,19 @@ class aeResponse
 	
 	public function __destruct()
 	{
-		if (($i = array_search($this->buffer, self::$buffers, true)) !== false)
+		if (($i = array_search($this->buffer, self::$buffers, true)) === false)
 		{
-			unset(self::$buffers[$i]);
+			return;
 		}
 		
+		if (count(self::$buffers) === 1)
+		{
+			$this->dispatch();
+		}
+		
+		unset(self::$buffers[$i]);
 	}
-
+	
 	protected static $http_errors = array(
 		// Client errors
 		400 => 'Bad Request',
